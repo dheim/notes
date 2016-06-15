202a7da25602794fb010ef010506c590c399d002
Thilo Holzer <th@holzer.com>
2016-06-15 19:27:53 +0200

    Note-APi => ES6

--- server/routes.js
+++ server/routes.js
@@ -1,59 +1,111 @@
 const express = require('express');
-const router  = express.Router();
 const config  = require('./config');
 const sqlite  = require('sqlite3');
 const path    = require('path');
+const db      = new sqlite.Database(path.join(__dirname, config.db));
 
-const db      = new sqlite.Database(path.join(__dirname, config.db));            
+class Note {
 
-router
-  .get('/note', (req, res) => {
-    db.all('select * from notes', (err, rows) => {
-      res.json(rows);
+  get(id = null) {
+    return new Promise( (resolve, reject) => {
+      if (id > 0) {
+        db.get('SELECT * FROM notes WHERE id = ?', id, (err, row) => {
+          if (err) reject(err);
+          resolve(row);
+        });
+      } else {
+        db.all('SELECT * FROM notes', (err, rows) => {
+          if (err) reject(err);
+          resolve(rows);
+        });
+      }
     });
-  })
-  .get('/note/:id', (req, res) => {
-    db.get(`select * from notes where id = ${req.params.id}`, (err, row) => {
-      res.json(row);
-    });
-  })
-  .post('/note', (req, res) => {
-    let note = req.body;
+  }
 
-    db.run(`insert into notes (title, description, due, finished) VALUES ($title, $description, $due, $finished)`, {
-      $title: note.title,
-      $description: note.description,
-      due: (note.due) ? note.due : null,
-      finished: (note.finished) ? note.finished : null
-    }, function (err) {
-      res.json(this.changes);
+  add(note) {
+    return new Promise( (resolve, reject) => {
+      db.run('INSERT INTO notes (title, description, due, finished) VALUES ($title, $description, $due, $finished)', {
+        $title: note.title,
+        $description: note.description,
+        $due: (note.due) ? note.due : null,
+        $finished: (note.finished) ? note.finished : null
+      }, (err) => {
+        if (err) reject(err);
+        resolve(this.lastID);
+      });
     });
-  })
-  .put('/note/:id', (req, res) => {
-    let note = req.body;
+  }
 
-    db.run(`update notes
-      set title = $title,
-          description = $description,
-          due = $due,
-          finished = $finished
-    where id = $id`, {
-      $id: note.id,
-      $title: note.title,
-      $description: note.description,
-      due: (note.due) ? note.due : null,
-      finished: (note.finished) ? note.finished : null
-    }, function (err) {
-      if(err) throw new Error(err);
-      res.json(this.changes);
+  update(note) {
+    return new Promise( (resolve, reject) => {
+      db.run('UPDATE notes SET title = $title, description = $description, due = $due, finished = $finished WHERE id = $id', {
+        $id: note.id,
+        $title: note.title,
+        $description: note.description,
+        $due: (note.due) ? note.due : null,
+        $finished: (note.finished) ? note.finished : null
+      }, (err) => {
+        if (err) reject(err);
+        resolve(this.lastID);
+      });
     });
+  }
 
-  })
-  .delete('/note/:id', (req, res) => {
-    db.run('delete from notes where id = $id', {$id: req.params.id}, (err) => {
-      if(err) throw new Error(err);
-      res.json(this.changes);
+  delete(id) {
+    return new Promise( (resolve, reject) => {
+      db.run('DELETE FROM notes WHERE id = $id', {
+        $id: id
+      }, (err) => {
+        if (err) reject(err);
+        resolve(this.lastID);
+      });
     });
+  }
+
+}        
+
+
+let noteRouter = (app) => {
+  const router  = express.Router();
+  const note = new Note();
+
+  app.io.on('connection', (socket) => {
   });
 
-module.exports = router;
+  router
+    .get('/note', (req, res) => {
+      note.get().then( (rows) => {
+        res.json(rows);
+      });
+    })
+    .get('/note/:id', (req, res) => {
+      note.get(req.params.id).then( (rows) => {
+        res.json(rows);
+      });
+    })
+    .post('/note', (req, res) => {
+      note.add(req.body).then( (lastId) => {
+        note.get(lastId).then( (row) => {
+          res.json(row);
+        });
+      }).catch( (err) => {
+        console.log(err);
+      });
+    })
+    .put('/note/:id', (req, res) => {
+      note.update(req.body).then( (lastId) => {
+        note.get(lastId).then( (row) => {
+          res.json(row);
+        });
+      });
+    })
+    .delete('/note/:id', (req, res) => {
+      note.delete(req.params.id).then( (lastId) => {
+        res.json(lastId);
+      });
+    });
+
+  return router;
+};
+
+module.exports = noteRouter;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express         = require('express');
 const bodyParser      = require('body-parser');
 const methodOverride  = require('method-override');
 const app             = express();
-
 const config          = require('./config');
+const server          = require('http').Server(app);
+app.io                = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/../public', {
   dotfiles: 'ignore'
@@ -11,7 +12,7 @@ app.use(express.static(__dirname + '/../public', {
 
 app
   .use(bodyParser.json())
-  .use('/api', require('./routes'))
+  .use('/api', require('./routes')(app))
   .listen(config.port, () => {
     console.log(`Server started on port ${config.port}`);
   });
